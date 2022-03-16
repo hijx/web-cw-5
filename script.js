ca83cfd8b5fe035516979a195f2f05b30629fee1
hijx <queen.spn.spn@gmail.com>
2022-03-16 20:37:10 +0300

    done

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ console.log(`أنا في الصف الالثاني عشر`);
 let budget = 20;
 console.log(budget > 30 && budget < 80);
 
-budget += 30
+budget += 30;
 console.log(budget > 30 && budget < 80);
+
+let grade = prompt(`ادخل درجتك`);
+if (grade >= 90) {
+  console.log(`لقد حصلت على امتياز 🥳`);
+} else if (grade >= 80) {
+  console.log(`لقد حصلت على جيد جداً🤩`);
+} else if (grade >= 70) {
+  console.log(`لقد حصلت على جيد🙂`);
+} else if (grade >= 60) {
+  console.log(`لقد حصلت على مقبول😕`);
+} else if (grade >= 50) {
+  console.log(`لقد حصلت على مقبول😕`);
+} else {
+  console.log(`راسب💔`);
+}
 
 // جافاسكربت ليست جافا
