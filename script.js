68cd29cfc3c19c563b89b3731cc6d7d5b48595c2
hijx <queen.spn.spn@gmail.com>
2022-03-16 19:35:16 +0300

    done

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ alert(`ماريا مشرف الشمري`);
 console.log(17);
 console.log(`أنا في الصف الالثاني عشر`);
 
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+
+budget += 30
+console.log(budget > 30 && budget < 80);
+
 // جافاسكربت ليست جافا
